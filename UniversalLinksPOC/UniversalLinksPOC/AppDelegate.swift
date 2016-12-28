eee4992606c42f6f2f5385f2df30f2d454d918e5
Kapil vallamkonda <kapilvallamkonda@gmail.com>
2016-12-28 10:29:14 -0600

    App configured for Universal links

--- UniversalLinksPOC/UniversalLinksPOC/AppDelegate.swift
+++ UniversalLinksPOC/UniversalLinksPOC/AppDelegate.swift
@@ -40,7 +40,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    func application(_ application: UIApplication, didUpdate userActivity: NSUserActivity) {
+        
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        return true
+    }
 }
+
 
